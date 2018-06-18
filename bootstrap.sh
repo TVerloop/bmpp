552b4a3fb473c8def858979fc115fd8195688d2a
TVerloop <t93.verloop@gmail.com>
2018-06-18 14:18:07 +0200

    docs: Added documentation.
    * Added file header.
    * added comments.
    * added commandline arguments.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,47 +1,86 @@
-# /bin/bash
+#! /bin/bash
+# -*- mode: sh-mode -*-
+#==============================================================================#
+# File:     bootstrap.sh
+# Author:   Tom Verloop   <T93.Verloop@gmail.com>
+# Version:  0.1
+# Date:     15-06-2018
+#
+# Bootstrap
+#
+# Sets up default build directory with the default toolchain and default build type.
+# Maskes symbolic link of the compile commands database.
+#==============================================================================#
 
+# Save positional arguments.
 POSITIONAL=()
+# Default build directory.
 BUILD_DIR=build
+# Default build type.
 BUILD_TYPE=Release
+# Default toolchain.
 TOOLCHAIN=gcc-arm-none-eabi.cmake
 
+# step through all commandline arguments.
 while [[ $# -gt 0 ]]; do
 
     key="$1"
 
     case $key in
+        # If build dir is specified.
         -d|--build-dir)
+            # Override default buil dir.
             BUILD_DIR="$2"
             shift
             shift
             ;;
+        # If build type is specified.
         -b|--build-type)
+            # Override default build type.
             BUILD_TYPE="$2"
             shift
             shift
             ;;
+        # If toolchain is specified.
         -t|--toolchain)
+            # Override default toolchain.
             TOOLCHAIN="$2"
             shift;
             shift;
             ;;
+        # Else
         *)
+            # Save positional flag.
             POSITIONAL+=("$1")
             shift
             ;;
     esac
 done
 
+# Apply positional arguments.
 set -- "${POSITIONAL[@]}"
 
+# If build dir does not exist create it.
 if [ ! -d ${BUILD_DIR} ]; then
     mkdir ${BUILD_DIR}
 fi
 
 cd ${BUILD_DIR}
 
-cmake -DCMAKE_TOOLCHAIN_FILE=../toolchains/${TOOLCHAIN} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+# call cmake to setup build directory.
+cmake -DCMAKE_TOOLCHAIN_FILE=toolchains/${TOOLCHAIN} \
+      -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+      ..
 
+rdm &
+RDM_PID=$!
+
+sleep .5
+
+rc -J .
+
+kill ${RDM_PID}
+
+# Move out of build directory.
 cd ..
 
-ln -sfr ${BUILD_DIR}/compile_commands.json compile_commands.json
